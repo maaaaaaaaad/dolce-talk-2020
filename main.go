b002d78df984fc0f5b97beeea3e1b7dd736079fb
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-19 08:19:09 +0000

    e

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	primeCount := 0
+	oddPrimeCount := 0
+
+	for i := 1; i <= 6; i++ {
+		if isPrime(i) {
+			primeCount++
+			if i%2 == 1 {
+				oddPrimeCount++
+			}
+		}
+	}
+
+	probability := float64(oddPrimeCount) / float64(primeCount)
+	fmt.Printf("한 개의 주사위를 던졌을 때, 나온 눈이 소수이면서 홀수일 확률은 %.2f입니다.\n", probability)
+}
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
