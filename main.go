34eb5f6e0f5ffd4f662a869b7825c1cf1bcd6752
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-19 08:22:58 +0000

    e

--- main.go
+++ main.go
@@ -2,31 +2,41 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	primeCount := 0
+// 	oddPrimeCount := 0
+
+// 	for i := 1; i <= 6; i++ {
+// 		if isPrime(i) {
+// 			primeCount++
+// 			if i%2 == 1 {
+// 				oddPrimeCount++
+// 			}
+// 		}
+// 	}
+
+// 	probability := float64(oddPrimeCount) / float64(primeCount)
+// 	fmt.Printf("한 개의 주사위를 던졌을 때, 나온 눈이 소수이면서 홀수일 확률은 %.2f입니다.\n", probability)
+// }
+
+// func isPrime(n int) bool {
+// 	if n <= 1 {
+// 		return false
+// 	}
+// 	for i := 2; i*i <= n; i++ {
+// 		if n%i == 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
 func main() {
-	primeCount := 0
-	oddPrimeCount := 0
+	pPositiveGivenRealPositive := 0.9
+	pPositiveGivenRealNegative := 0.2
+	pRealPositive := 0.6
 
-	for i := 1; i <= 6; i++ {
-		if isPrime(i) {
-			primeCount++
-			if i%2 == 1 {
-				oddPrimeCount++
-			}
-		}
-	}
-
-	probability := float64(oddPrimeCount) / float64(primeCount)
-	fmt.Printf("한 개의 주사위를 던졌을 때, 나온 눈이 소수이면서 홀수일 확률은 %.2f입니다.\n", probability)
-}
-
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
+	pPositive := pPositiveGivenRealPositive*pRealPositive + pPositiveGivenRealNegative*(1.0-pRealPositive)
+	pRealPositiveGivenPositive := (pPositiveGivenRealPositive * pRealPositive) / pPositive
+	fmt.Printf("이 과일이 실제로 상품일 때, 상품으로 판정될 확률은 %.2f입니다.\n", pRealPositiveGivenPositive)
 }
