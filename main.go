3e08034096a4b0e703a0e7c4d3567c878aac1eb2
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-20 05:15:44 +0000

    e

--- main.go
+++ main.go
@@ -2,41 +2,55 @@ package main
 
 import "fmt"
 
-// func main() {
-// 	primeCount := 0
-// 	oddPrimeCount := 0
+type Graph map[int][]int
 
-// 	for i := 1; i <= 6; i++ {
-// 		if isPrime(i) {
-// 			primeCount++
-// 			if i%2 == 1 {
-// 				oddPrimeCount++
-// 			}
-// 		}
-// 	}
+func dfs(node int, parent int, graph Graph, values []int, sums []int) {
+	sums[node] = values[node-1]
+	for _, child := range graph[node] {
+		if child == parent {
+			continue
+		}
+		dfs(child, node, graph, values, sums)
+		sums[node] += sums[child]
+	}
+}
 
-// 	probability := float64(oddPrimeCount) / float64(primeCount)
-// 	fmt.Printf("한 개의 주사위를 던졌을 때, 나온 눈이 소수이면서 홀수일 확률은 %.2f입니다.\n", probability)
-// }
-
-// func isPrime(n int) bool {
-// 	if n <= 1 {
-// 		return false
-// 	}
-// 	for i := 2; i*i <= n; i++ {
-// 		if n%i == 0 {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+func Solution(values []int, edges [][]int, queries [][]int) []int {
+	n := len(values)
+	graph := make(Graph)
+	for _, edge := range edges {
+		u, v := edge[0], edge[1]
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
+	}
+	sums := make([]int, n+1)
+	dfs(1, 0, graph, values, sums)
+	answer := make([]int, 0, len(queries))
+	for _, query := range queries {
+		u, w := query[0], query[1]
+		if w == -1 {
+			answer = append(answer, sums[u])
+		} else {
+			diff := w - values[u-1]
+			values[u-1] = w
+			for u != 1 {
+				sums[u] += diff
+				parent := graph[u][0]
+				if parent == query[1] {
+					parent = graph[u][1]
+				}
+				u = parent
+			}
+			sums[1] += diff
+		}
+	}
+	return answer
+}
 
 func main() {
-	pPositiveGivenRealPositive := 0.9
-	pPositiveGivenRealNegative := 0.2
-	pRealPositive := 0.6
-
-	pPositive := pPositiveGivenRealPositive*pRealPositive + pPositiveGivenRealNegative*(1.0-pRealPositive)
-	pRealPositiveGivenPositive := (pPositiveGivenRealPositive * pRealPositive) / pPositive
-	fmt.Printf("이 과일이 실제로 상품일 때, 상품으로 판정될 확률은 %.2f입니다.\n", pRealPositiveGivenPositive)
+	values := []int{1, 2, 3, 4, 5, 6}
+	edges := [][]int{{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 6}}
+	queries := [][]int{{1, -1}, {2, 4}, {6, 1}, {4, 0}}
+	answer := Solution(values, edges, queries)
+	fmt.Println(answer)
 }
